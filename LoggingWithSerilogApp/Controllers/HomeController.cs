5f82c17b4e55ed4ed7bdac72f086aba52597b89e
Khushbu Pandya <khushbu1894@gmail.com>
2021-09-16 13:00:51 +0530

    Merge changes of logging with Serilog

---
+++ LoggingWithSerilogApp/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using LoggingWithSerilogApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoggingWithSerilogApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                decimal result = Divide(5, 0);
+            }
+            catch (DivideByZeroException ex)
+            {
+                _logger.LogWarning(ex, "An exception occured while dividing two numbers");
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public decimal Divide(decimal a, decimal b)
+        {
+            _logger.LogInformation("Parameter 1: " + a);
+            _logger.LogInformation("Parameter 2: " + b);
+
+            decimal result = 0;
+
+            try
+            {
+                result = a / b;
+            }
+            catch (DivideByZeroException ex)
+            {
+                _logger.LogWarning(ex, "You cannot divide by zero.");
+                throw ex;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LoggingWithSerilogApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LoggingWithSerilogApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
